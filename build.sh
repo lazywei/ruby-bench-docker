1f262bafc6b40e11022231a3b1f45af347d9a297
Bert Chang <jrweizhang@gmail.com>
2014-05-08 14:05:48 +0800

    Add building ruby script.

--- build.sh
+++ build.sh
@@ -10,11 +10,27 @@ function run_on_rb_bench {
   commit_to_image
 }
 
+function run_in_ruby_dir {
+  docker run -w /ruby --name=rb-bench rb-bench "$@"
+  commit_to_image
+}
+
 docker pull ubuntu:13.10
 
 docker run --name=rb-bench ubuntu:13.10 apt-get update
 commit_to_image
 
+run_on_rb_bench apt-get install -y build-essential
+run_on_rb_bench apt-get install -y autoconf
 run_on_rb_bench apt-get install -y git
+run_on_rb_bench apt-get install -y ruby
+run_on_rb_bench apt-get install -y bison
 
 run_on_rb_bench git clone https://github.com/ruby/ruby.git
+
+run_in_ruby_dir autoconf
+run_in_ruby_dir ./configure
+run_in_ruby_dir make
+run_in_ruby_dir make install
+
+run_on_rb_bench /usr/local/bin/ruby -v
