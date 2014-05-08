93d91afe472744257ca52e233fa080abf4f3f3f0
Bert Chang <jrweizhang@gmail.com>
2014-05-08 12:52:41 +0800

    Wrap run & commit.

--- build.sh
+++ build.sh
@@ -5,10 +5,16 @@ function commit_to_image {
   docker rm rb-bench
 }
 
+function run_on_rb_bench {
+  docker run --name=rb-bench rb-bench "$@"
+  commit_to_image
+}
+
 docker pull ubuntu:13.10
 
 docker run --name=rb-bench ubuntu:13.10 apt-get update
 commit_to_image
 
-docker run --name=rb-bench rb-bench apt-get install -y git
-commit_to_image
+run_on_rb_bench apt-get install -y git
+
+run_on_rb_bench git clone https://github.com/ruby/ruby.git
