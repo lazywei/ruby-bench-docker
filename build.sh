a02adaf2e027fb124a93be2ea51261023c8104b6
Bert Chang <jrweizhang@gmail.com>
2014-05-22 16:24:12 +0800

    Control ruby sha from build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 IMAGE=$(docker images | grep "rb_bench/base " |  awk '{print $3}')
 if [[ -z $IMAGE ]]; then
   echo "rb_bench/base image not found... Try to build from ./dockerfiles/Base"
-  docker build -t rb_bench/base - < $PWD/dockerfiles/Base
+  docker build -t rb_bench/base - < dockerfiles/Base
 fi
 
-echo $PWD
-
-docker build -t rb_bench/ruby:2.2.0-dev - < $PWD/dockerfiles/Ruby
+cat dockerfiles/Ruby | sed -e "s/\${sha}/5b629a7b8a344be1683a137f48d062634046f661/g" | docker build -t rb_bench/ruby -
