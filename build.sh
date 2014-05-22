3a87c5364e2d9a52a769105210cf45773c9e9b2b
Bert Chang <jrweizhang@gmail.com>
2014-05-22 16:53:04 +0800

    Tag docker images by git SHA.

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-IMAGE=$(docker images | grep "rb_bench/base " |  awk '{print $3}')
-if [[ -z $IMAGE ]]; then
+RUBY_SHA=5b629a7b8a344be1683a137f48d062634046f661
+
+BASE_IMG=$(docker images | grep "rb_bench/base " |  awk '{print $3}')
+if [[ -z $BASE_IMG ]]; then
   echo "rb_bench/base image not found... Try to build from ./dockerfiles/Base"
   docker build -t rb_bench/base - < dockerfiles/Base
 fi
 
-cat dockerfiles/Ruby | sed -e "s/\${sha}/5b629a7b8a344be1683a137f48d062634046f661/g" | docker build -t rb_bench/ruby -
+
+RUBY_IMG=$(docker images | grep "^rb_bench/ruby\s*${RUBY_SHA:0:5}" |  awk '{print $3}')
+if [[ -z $RUBY_IMG ]]; then
+  echo "rb_bench/ruby:${RUBY_SHA:0:5} image not found... Try to build from ./dockerfiles/Ruby"
+  cat dockerfiles/Ruby | sed -e "s/\${sha}/${RUBY_SHA}/g" | docker build -t rb_bench/ruby:${RUBY_SHA:0:5} -
+fi
