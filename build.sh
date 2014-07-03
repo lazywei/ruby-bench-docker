972eb742b028821bcfeb91ee7a29b36eaa20149c
Bert Chang <jrweizhang@gmail.com>
2014-07-03 16:38:04 +0800

    Change docker images name.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 RUBY_SHA=5b629a7b8a344be1683a137f48d062634046f661
 
-BASE_IMG=$(docker images | grep "rb_bench/base " |  awk '{print $3}')
+BASE_IMG=$(docker images | grep "lazywei/rb_bench_base " |  awk '{print $3}')
 if [[ -z $BASE_IMG ]]; then
   echo "rb_bench/base image not found... Try to build from ./dockerfiles/Base"
   docker build -t rb_bench/base - < dockerfiles/Base
