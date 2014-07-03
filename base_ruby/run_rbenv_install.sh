39b3f175fb751dc9f89ec1e2f91c65af959b6099
Bert Chang <jrweizhang@gmail.com>
2014-07-03 17:28:28 +0800

    Add a build script for base_ruby.

---
+++ base_ruby/run_rbenv_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RUBY_SHA=5b629a7b8a344be1683a137f48d062634046f661
+
+echo 'install_package "yaml-0.1.6" "http://pyyaml.org/download/libyaml/yaml-0.1.6.tar.gz#5fe00cda18ca5daeb43762b80c38e06e" --if needs_yaml' > ruby-${RUBY_SHA}
+echo 'install_package "ruby-5b629a7b8a344be1683a137f48d062634046f661" "https://github.com/ruby/ruby/archive/SHA.tar.gz" ldflags_dirs autoconf standard verify_openssl' | sed -e "s/SHA/${RUBY_SHA}/g" >> ruby-${RUBY_SHA}
+
+source /.bashrc
+rbenv install ruby-${RUBY_SHA}
+rbenv global ruby-${RUBY_SHA}
