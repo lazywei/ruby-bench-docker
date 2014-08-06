18a9d11b72f9687f06e3c0f24797f10867f29656
Bert Chang <jrweizhang@gmail.com>
2014-08-06 23:21:37 +0800

    Update curl parameters

--- discourse_bench/runner.sh
+++ discourse_bench/runner.sh
@@ -11,4 +11,4 @@ curl -i -H 'Content-Type: application/json' \
   -H 'Accept: application/json,application/vnd.ruby_bench;ver=1' \
   -H 'Authorization: Token token="123"' \
   -X POST http://localhost:3000/api/results \
-  -d "{\"run\":{\"ruby_version\":\"2.0.0-p247\",\"git_hash\":\"c0a04be8ddcadcd89c02bc135e4b288f6d936704\",\"date\":\"2013-12-18T12:09:16+00:00\",\"results_attributes\":[{\"benchmark\":\"some_bench\",\"score\":\"$elapsed\"}]}}"
+  -d "{\"run\":{\"ruby_version\":\"2.0.0-p247\",\"git_hash\":\"c0a04be8ddcadcd89c02bc135e4b288f6d936704\",\"rails_version\": \"4.0.0\",\"rails_git_hash\":\"7c4bfe\",\"date\":\"2013-12-18T12:09:16+00:00\",\"results_attributes\":[{\"benchmark\":\"some_bench\",\"score\":\"$elapsed\"}]}}"
